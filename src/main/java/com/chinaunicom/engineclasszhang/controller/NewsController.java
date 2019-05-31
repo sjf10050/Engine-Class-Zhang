dd7471d2e18ad2f08cf0eb94d408c4a192e77065
sjf10050 <sjf10050@outlook.com>
2019-05-31 13:42:03 +0800

    news

--- src/main/java/com/chinaunicom/engineclasszhang/controller/NewsController.java
+++ src/main/java/com/chinaunicom/engineclasszhang/controller/NewsController.java
@@ -1,6 +1,7 @@
 package com.chinaunicom.engineclasszhang.controller;
 
 import com.chinaunicom.engineclasszhang.entity.News;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
